827c6941405e21a7281de8c3ced4f93c595788ed
jho951 <jho951@naver.com>
2022-09-19 18:12:48 +0900

    # 리액트 설정 끝

---
+++ frontchat/src/components/Chat.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Chat() {
+  return <>채팅 페이지 입니다.</>;
+}
---
+++ frontchat/src/components/Join.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Join() {
+  return <>메인페이지</>;
+}
+export default Join;
