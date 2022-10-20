78ca4b7faac4f62cd31f633a4e6f5ccb549d7632
jho951 <jho951@naver.com>
2022-10-20 22:22:20 +0900

    #1 feat state 형식

--- frontchat/src/App.js
+++ frontchat/src/App.js
@@ -1,5 +1,7 @@
-import react from "react";
+import { useState } from "react";
 const App = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   return (
     <>
       <h1>List</h1>
